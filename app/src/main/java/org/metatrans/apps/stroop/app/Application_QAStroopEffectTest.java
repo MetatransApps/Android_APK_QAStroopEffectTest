5f531515a57665e562539fb6e3cd194fcfe0a7c7
MetatransApps <kitopchiyski@gmail.com>
2021-09-06 23:17:07 +0300

    Unify methods & members order

--- app/src/main/java/org/metatrans/apps/stroop/app/Application_QAStroopEffectTest.java
+++ app/src/main/java/org/metatrans/apps/stroop/app/Application_QAStroopEffectTest.java
@@ -1,7 +1,6 @@
 package org.metatrans.apps.stroop.app;
 
 
-import org.metatrans.apps.stroop.app.Application_EC;
 import org.metatrans.apps.stroop.cfg.ads.AdsConfigurations_EC;
 import org.metatrans.commons.ads.api.IAdsConfigurations;
 import org.metatrans.commons.cfg.appstore.IAppStore;
@@ -10,17 +9,17 @@ import org.metatrans.commons.cfg.appstore.IAppStore;
 public class Application_QAStroopEffectTest extends Application_EC {
 
 
-	@Override
-	public IAppStore getAppStore() {
-		return IAppStore.OBJ_FDROID_OFFICIAL;
-	}
-
-
 	protected IAdsConfigurations adsConfigurations = new AdsConfigurations_EC(this);
 
 
 	@Override
 	public IAdsConfigurations getAdsConfigurations() {
 		return adsConfigurations;
+	}
+
+
+	@Override
+	public IAppStore getAppStore() {
+		return IAppStore.OBJ_FDROID_OFFICIAL;
 	}
 }
